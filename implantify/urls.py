f2759a3d60e6d0b7a181785052aabbf63357b528
letsgo247 <letsgo247@hanmail.net>
2020-09-12 01:09:51 +0900

    .idea ignore

--- implantify/urls.py
+++ implantify/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from identifier import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('^$',)
+    url('^$', views.index, name='index'),
+
 ]
