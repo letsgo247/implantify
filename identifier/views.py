99f6601344a00510a90734c0940217c28ffa2ff8
letsgo247 <letsgo247@hanmail.net>
2020-09-14 21:03:11 +0900

    till bootstrap

--- identifier/views.py
+++ identifier/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 
+from django.core.files.storage import FileSystemStorage
+
 # Create your views here.
 
 def index(request):
     context={'a':1}
-    return render(request, 'index.html', context)
+    return render(request, 'identifier/index.html', context)
+
+def cover(request):
+    context={'a':1}
+    return render(request, 'identifier/cover.html', context)
+
+
+def predictImage(request):
+    # print('request=',request)
+    # print('request.POST=',request.POST)
+    print('request.POST.dict()=',request.POST.dict())
+    # print('request.FILES=',request.FILES)
+    # print("request.FILES['filePath']=",request.FILES['filePath'])
+    fileObj=request.FILES['filePath']
+    fs=FileSystemStorage()
+    # print(fileObj.name)
+    filePathName=fs.save(fileObj.name,fileObj)
+    print('filaPathName=', filePathName)
+    # filePathName=fs.url(filePathName)
+    # print('filaPathName2=', filePathName)
+
+    context={'filePathName':filePathName}
+    return render(request, 'identifier/cover.html', context)
--- implantify/settings.py
+++ implantify/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))      # 지금 이 파일의 경로의 상위의 상위 디렉토리까지의 path
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
